4e0b6f268b17e28a885c4d84664096e086366f04
Ambareen09 <ambubushra@gmail.com>
2023-02-06 22:22:11 +0530

    static root added

--- imex/imex/settings.py
+++ imex/imex/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
